4da0fb1092fc9e7ea6e7550f72c3dde800c671ee
WilsonnnTan <wilsonangelietann@gmail.com>
2026-09-16 20:00:09 +0700

    up

--- task-1/backup-script.sh
+++ task-1/backup-script.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
-# Global Var
-WATCHED_FOLDER="$(pwd)/hpc-dir"
-BACKUP_FOLDER="$(pwd)/backup"
+# Implementation of basic incremental backup script
+# This script doesnt support resuming an existing backup
+# Everytime this script run, it will generate a new backup folder
 
 # Config Var
 BACKUP_NAME="your-project-name"
 
+# Global Var
+WATCHED_FOLDER="$(pwd)/hpc-dir"
+BACKUP_FOLDER="$(pwd)/backup/$BACKUP_NAME"
+LATEST_BACKUP=""
+
 # Full backup needed for init
 full_backup() {
   timestamp=$(date -u +%Y%m%d_%H%M%S_UTC)
-  target="$BACKUP_FOLDER/$BACKUP_NAME/full-backup_$timestamp"
+  target="$BACKUP_FOLDER/full-backup_$timestamp"
   mkdir -p "$target"
   cp -r "$WATCHED_FOLDER/." "$target"
+  LATEST_BACKUP="$target"
 }
 
-# Diff Checker
+# Directory diff checker
 check_dir_diff() {
   echo "check dir diff"
 }
 
+# File diff checker
 check_file_diff() {
+  diff 
   echo "check file diff"
 }
 
